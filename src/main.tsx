f775019cf1b162355d1fe200ad4233f9971abe85
Matheus Victor <hffmatheus@gmail.com>
2022-10-22 20:52:39 -0300

    chore: install class-transfomer

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
+import 'reflect-metadata';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './Dog/presentation/Home/HomePage';
 import './index.css';
+import { HomeContextProvider } from './Dog/application/homeContext';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <HomePage />,
+    element: (
+      <HomeContextProvider>
+        <HomePage />
+      </HomeContextProvider>
+    ),
   },
 ]);
 
