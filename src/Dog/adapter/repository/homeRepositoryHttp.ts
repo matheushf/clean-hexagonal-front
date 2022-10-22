bba03870be080fb023f32b3902ba5e3b2829fedd
Matheus Victor <hffmatheus@gmail.com>
2022-10-22 20:52:01 -0300

    chore: more repository

--- src/Dog/adapter/repository/homeRepositoryHttp.ts
+++ src/Dog/adapter/repository/homeRepositoryHttp.ts
@@ -1,16 +1,40 @@
+import { plainToInstance } from 'class-transformer';
 import { HomeRepositoryI } from '../../domain/ports/HomeRepository';
 import { HttpI } from '../../../shared/domain/ports/http';
+import { DogE, FavoriteDogE } from '../../domain/entity';
 
 export function HomeRepositoryHttp(http: HttpI): HomeRepositoryI {
-  const commonConfig = { baseURL: 'https://api.thedogapi.com/v1/' };
+  const commonConfig = {
+    baseURL: 'https://api.thedogapi.com/v1/',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-api-key': 'live_zrycHb91rLeX2wTEovXkDqoQP2KcaBtfh5ZS7BJQXmZPDzpQl3fgxgTuFsJUWAO4',
+    },
+  };
 
   function getDogs() {
-    return http.get('/breeds?limit=10&page=0', commonConfig);
+    return http
+      .get('/breeds?limit=10&page=0', commonConfig)
+      .then((dogs) => plainToInstance(DogE, dogs));
   }
 
-  function saveFavouriteDogImage() {
-    return http.post('/favourites', {}, commonConfig);
+  function getFavouriteDogs() {
+    return http.get('/favourites', commonConfig);
   }
 
-  return { getDogs, saveFavouriteDogImage };
+  function saveFavouriteDog(dog: DogE) {
+    return http.post(
+      '/favourites',
+      {
+        image_id: dog.image.id,
+      },
+      commonConfig,
+    );
+  }
+
+  function removeFavouriteDog(favoriteDog: FavoriteDogE) {
+    return http.delete(`/favourites/${favoriteDog.id}`, commonConfig);
+  }
+
+  return { getDogs, saveFavouriteDog, getFavouriteDogs, removeFavouriteDog };
 }
--- src/Dog/domain/ports/HomeRepository.ts
+++ src/Dog/domain/ports/HomeRepository.ts
@@ -1,6 +1,9 @@
-import { DogI } from '../entity/dog';
+import { FavoriteDogE } from '../entity';
+import { DogE } from '../entity/dog';
 
 export interface HomeRepositoryI {
-  getDogs: () => Promise<DogI[]>;
-  saveFavouriteDogImage: () => Promise<any>;
+  getDogs: () => Promise<DogE[]>;
+  saveFavouriteDog: (dog: DogE) => Promise<any>;
+  removeFavouriteDog: (dog: FavoriteDogE) => Promise<any>;
+  getFavouriteDogs: () => Promise<any>;
 }
