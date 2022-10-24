69a0b12c2e40740e6ee9dc97da37cc9d90e024f5
Matheus Victor <hffmatheus@gmail.com>
2022-10-24 19:39:34 -0300

    chore: add i18n and formik

---
+++ src/Dog/domain/locales/en-US/index.ts
@@ -0,0 +1,7 @@
+import home from './home.json';
+import addDog from './add-dog.json';
+
+export const enUS = {
+  home,
+  addDog,
+};
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,15 @@ import 'reflect-metadata';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { initReactI18next } from 'react-i18next';
 import HomePage from './Dog/presentation/Home/HomePage';
 import './index.css';
 import { HomeContextProvider } from './Dog/application/home/homeContext';
 import DetailsPage from './Dog/presentation/Details/DetailsPage';
+import { initI18n } from './Dog/infra/i18n';
+import { AddDogPage } from './Dog/presentation/AddDog/AddDogPage';
+
+initI18n([initReactI18next]);
 
 const router = createBrowserRouter([
   {
@@ -19,6 +24,10 @@ const router = createBrowserRouter([
   {
     path: '/dog/:dogId',
     element: <DetailsPage />,
+  },
+  {
+    path: '/new-dog',
+    element: <AddDogPage />,
   },
 ]);
 
