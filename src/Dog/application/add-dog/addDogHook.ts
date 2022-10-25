9a66e19913812ea70689e0fb1cb035f347843b91
Matheus Victor <hffmatheus@gmail.com>
2022-10-25 18:59:47 -0300

    chore: adjusts

--- src/Dog/application/add-dog/addDogHook.ts
+++ src/Dog/application/add-dog/addDogHook.ts
@@ -1,16 +1,31 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Repository } from '../../adapter/repository';
 import { DogE } from '../../domain/entity';
+import { DogFormValuesT } from '../../domain/schema/dogValidationSchema';
 
 export function useAddDog() {
-  const [dogs, setDogs] = useState<Partial<DogE>[]>();
+  const [dogs, setDogs] = useState<Partial<DogE>[]>([]);
 
-  function onSave(dog: Partial<DogE>, formik: any) {
+  function onSave(dog: DogFormValuesT, formik: any) {
     formik.resetForm();
 
-    const response = Repository.dog.addDog(dog);
-    setDogs([...response]);
+    if (dog.saveOn === 'memory') {
+      const response = Repository.dog.addDogMemory(dog);
+      setDogs([...dogs, ...response]);
+    } else {
+      const response = Repository.dog.addDogStorage(dog);
+      setDogs([...response]);
+    }
   }
+
+  function fetchDogs() {
+    const response = Repository.dog.getDogsLocalStorage();
+    setDogs(response);
+  }
+
+  useEffect(() => {
+    fetchDogs();
+  }, []);
 
   return { dogs, onSave };
 }
