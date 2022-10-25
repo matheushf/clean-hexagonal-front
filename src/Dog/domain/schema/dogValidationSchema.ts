9eae81d15fb1bba695a0730a91b92933efbb8a34
Matheus Victor <hffmatheus@gmail.com>
2022-10-25 18:59:12 -0300

    chore: add a schema validation with zod

--- src/Dog/domain/schema/dogValidationSchema.ts
+++ src/Dog/domain/schema/dogValidationSchema.ts
@@ -1,5 +1,17 @@
 import { z } from 'zod';
+import { DogE } from '../entity';
 
 export const DogSchema = z.object({
-  name: z.string().min(4),
+  name: z.string().min(2),
+  breed: z.string().min(4),
 });
+
+export type DogFormValuesT = Partial<DogE> & {
+  saveOn: 'memory' | 'storage';
+};
+
+export const dogInitialValues: DogFormValuesT = {
+  name: '',
+  breed: '',
+  saveOn: 'memory',
+};
--- src/shared/infra/schemaValidator.ts
+++ src/shared/infra/schemaValidator.ts
@@ -2,6 +2,7 @@ import { z, ZodError } from 'zod';
 
 export function validateSchema(values: any, schema: z.ZodObject<any>) {
   try {
+    console.log('oi ', values);
     return !!schema.parse(values);
   } catch (error: any) {
     if (error instanceof ZodError) {
