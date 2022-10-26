b14376c65f487b9ad022123401254b4822c5f5bd
Matheus Victor <hffmatheus@gmail.com>
2022-10-26 09:33:10 -0300

    chore: install grommet icons

--- src/Dog/adapter/axios.ts
+++ src/Dog/adapter/axios.ts
@@ -6,7 +6,7 @@ export const AxiosAdapter: HttpI = {
     return axios.get(path, config).then((response) => response.data);
   },
   post(path, params?, config?) {
-    return axios.post(path, params, config);
+    return axios.post(path, params, config).then((response) => response.data);
   },
   put(path, params?, config?) {
     return axios.put(path, params, config);
