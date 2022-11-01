e7974954ad61e8230e560c4eaa0556d8aea63176
Matheus Victor <hffmatheus@gmail.com>
2022-11-01 20:36:16 -0300

    chore: main on application folder

--- packages/dog/src/application/main/main.tsx
+++ packages/dog/src/application/main/main.tsx
@@ -4,12 +4,12 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { initReactI18next } from 'react-i18next';
 import { Grommet } from 'grommet';
-import HomePage from '../presentation/Home/HomePage';
+import HomePage from '../../presentation/Home/HomePage';
 import './index.css';
-import { HomeContextProvider } from './home/homeContext';
-import DetailsPage from '../presentation/Details/DetailsPage';
-import { initI18n } from '../infra/i18n';
-import { AddDogPage } from '../presentation/AddDog/AddDogPage';
+import { HomeContextProvider } from '../home/homeContext';
+import DetailsPage from '../../presentation/Details/DetailsPage';
+import { initI18n } from '../../infra/i18n';
+import { AddDogPage } from '../../presentation/AddDog/AddDogPage';
 
 initI18n([initReactI18next]);
 
