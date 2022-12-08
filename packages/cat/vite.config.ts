866efda690ee79e3f6142118b3a63cbcd85daffb
Matheus Victor <hffmatheus@gmail.com>
2022-12-07 22:17:36 -0300

    chore: add partytown to cat vue microfront

--- packages/cat/vite.config.ts
+++ packages/cat/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from "path";
+import { partytownVite } from "@builder.io/partytown/utils";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue(), partytownVite({
+    dest: path.join(__dirname, "dist", "~partytown"),
+  })]
 })
